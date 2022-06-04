acbb8b706c70abefe2564f47eedbb2953619d026
RosarioRu <mariarruval@gmail.com>
2022-06-04 12:41:56 -0700

    Change embedded expression to USD since that will not be changing.

--- src/currencies-service.js
+++ src/currencies-service.js
@@ -1,10 +1,10 @@
 //business logic with class example below:
 
 export default class Currencies {
-  static getCurrency(dollars) {
+  static getCurrency(amount, changeTo) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${EUR}/${changeTo}/${amount}`;
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${changeTo}/${amount}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
