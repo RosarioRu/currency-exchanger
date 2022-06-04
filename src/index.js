72bc7fec6ae578f49f98061b4ad73a9057a86b65
RosarioRu <mariarruval@gmail.com>
2022-06-04 11:33:07 -0700

    Update import statement so it pulls Currencies class from currencies-service.js file.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Currencies from './js/currencies.js';
+import Currencies from './currencies-service
 
 $(document).ready(function() {
   //UI logic goes here!
