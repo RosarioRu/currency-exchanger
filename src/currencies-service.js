acb091e0b3494209f1cc57f89cad4b00a337250a
RosarioRu <mariarruval@gmail.com>
2022-06-04 11:31:41 -0700

    Add Currencies class and function to get the currency based on dollars entered. Wrap API request in a promise.

---
+++ src/currencies-service.js
@@ -0,0 +1,22 @@
+//business logic with class example below:
+
+export default class Currencies {
+  static getCurrency(dollars) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${EUR}/${GBP}/${AMOUNT}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
+  
+
+    
