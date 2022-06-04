88002878262b386f0d79a8e50fa884c69cd3015b
RosarioRu <mariarruval@gmail.com>
2022-06-04 12:12:23 -0700

    Add document ready function that takes user inputs and saves as variables to be used later in API call.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,35 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Currencies from './currencies-service
+import Currencies from './currencies-service.js';
 
 $(document).ready(function() {
-  //UI logic goes here!
+  $("form#user-inputs").submit(function() {
+    event.preventDefault();
+    const amount = $("#amount").val();
+    $("#amount").val("");
+    const changeTo = $("#currency-selected").val();
+    $("#currency-selected").val("");
   });
+  
+});
+
+
+
+// $('#weatherLocation').click(function() {
+//   let city = $('#location').val();
+//   $('#location').val("");
+
+//   let promise = new Promise(function(resolve, reject) {
+//     let request = new XMLHttpRequest();
+//     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
+//     request.onload = function() {
+//       if (this.status === 200) {
+//         resolve(request.response);
+//       } else {
+//         reject(request.response);
+//       }
+//     }
+//     request.open("GET", url, true);
+//     request.send();
+//   });
